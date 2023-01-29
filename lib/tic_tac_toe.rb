85a664b8207da89f92d12275a0faebcdd3ce6bd2
Zalman <moishkugel@gmail.com>
2023-01-29 15:07:27 -0500

    commit tree e67326391e42603f124ba92dba27ee52017c4ed4

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,63 @@
+
+
+class TicTacToe
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
+
+  def initialize
+    @board =  [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n-----------\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n-----------\n #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(index)
+    index.to_i - 1
+  end
+
+  def move(index, token)
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
+  end
+
+  def valid_move?(index)
+    !(@board[index].nil?) && !(position_taken?(index))
+  end
+
+  def turn_count
+     @board.reject {|index| index == " "}.count 
+  end
+
+  def current_player
+    if turn_count.even?
+      "X"
+    else
+      "O"
+    end
+  end
+
+  def turn
+    puts "Please enter a number (1-9):"
+    num = gets.strip
+    index = input_to_index(num)
+    if valid_move?(index)
+      token = current_player
+      move(index, token)
+    else
+      turn
+    end
+    display_board
+  end
+end
